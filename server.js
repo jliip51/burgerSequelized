4a50ab75e44bddb388728b97e4e508b2ff7e3ffb
jliip51 <jliip51@gmail.com>
2017-08-21 19:23:25 -0400

    sequelize config, models, migration

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
+var db = require('./models');
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
